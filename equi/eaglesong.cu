9f262440541dd5c147cdac68ab864541c04fb6ba
Atbnsanw <850191536@qq.com>
2019-09-05 18:03:13 +0800

    first

---
+++ equi/eaglesong.cu
@@ -0,0 +1,281 @@
+/// System includes
+#include <stdio.h>
+#include <assert.h>
+
+// CUDA runtime
+#include <cuda_runtime.h>
+#include <cuda.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stddef.h>
+#include "portable_endian.h"
+#include "eaglesong.h"
+
+#define INPUT_LEN (32)
+#define N ((INPUT_LEN+8+1)+3) >> 2
+#define M (INPUT_LEN >> 2)
+#define OUTPUT_LEN 32
+#define THREADS_PER_BLOCK  (512)
+#define MAX_HASH_NUM (1<<30)
+#define MAX_GPU_NUM (1024)
+#define HASH_NUM (1<<27)
+
+#define DELIMITER (0x06)
+
+#define ROUND (43)
+
+
+#define ROL32(a,b) (((a)<<(b))|((a)>>(32-(b))))
+#define ROL_ADD(a,b) a += b; a = ROL32(a, 8); b = ROL32(b, 24) + a;
+
+#define EaglesongPermutation() \
+{ \
+	for(int i = 0, j=0; i < ROUND ; ++i, j+=16) { \
+		tmp = s0 ^ s4 ^ s12 ^ s15; s0 = tmp^s5 ^ s6 ^ s7; s1 = tmp^s1 ^ s8 ^ s13; \
+		tmp = s1 ^ s2 ^ s6 ^ s14; s2 = tmp^s7 ^ s8 ^ s9; s3 = tmp^s3 ^ s10 ^ s15; \
+		tmp = s0 ^ s3 ^ s4 ^ s8; s4 = tmp^s9 ^ s10 ^ s11; s5 = tmp^s1 ^ s5 ^ s12; \
+		tmp = s2 ^ s5 ^ s6 ^ s10; s6 = tmp^s11 ^ s12 ^ s13; s7 = tmp^s3 ^ s7 ^ s14; \
+		tmp = s4 ^ s7 ^ s8 ^ s12; s8 = tmp^s13 ^ s14 ^ s15; s9 = tmp^s0 ^ s5 ^ s9; \
+		tmp = s6 ^ s9 ^ s10 ^ s14; s10 = tmp^s0 ^ s1 ^ s15; s11 = tmp^s2 ^ s7 ^ s11; \
+		tmp = s0 ^ s8 ^ s11 ^ s12; s12 = tmp^s1 ^ s2 ^ s3; s13 = tmp^s4 ^ s9 ^ s13; \
+		tmp = s3 ^ s5 ^ s13 ^ s14; s14 = tmp^s2 ^ s4 ^ s10; s15 = tmp^s0 ^ s1 ^ s6 ^ s7 ^ s8 ^ s9 ^ s15; \
+		s0 ^= ROL32(s0, 2) ^ ROL32(s0, 4) ^ gpu_injection_constants[(j ^ 0)];                    \
+		s1 ^= ROL32(s1, 13) ^ ROL32(s1, 22) ^ gpu_injection_constants[(j ^ 1)];                  \
+		ROL_ADD(s0, s1);                                                                      \
+		s2 ^= ROL32(s2, 4) ^ ROL32(s2, 19) ^ gpu_injection_constants[(j ^ 2)];                   \
+		s3 ^= ROL32(s3, 3) ^ ROL32(s3, 14) ^ gpu_injection_constants[(j ^ 3)];                   \
+		ROL_ADD(s2, s3);                                                                      \
+		s4 ^= ROL32(s4, 27) ^ ROL32(s4, 31) ^ gpu_injection_constants[(j ^ 4)];                  \
+		s5 ^= ROL32(s5, 3) ^ ROL32(s5, 8) ^ gpu_injection_constants[(j ^ 5)];                    \
+		ROL_ADD(s4, s5);                                                                      \
+		s6 ^= ROL32(s6, 17) ^ ROL32(s6, 26) ^ gpu_injection_constants[(j ^ 6)];                  \
+		s7 ^= ROL32(s7, 3) ^ ROL32(s7, 12) ^ gpu_injection_constants[(j ^ 7)];                   \
+		ROL_ADD(s6, s7);                                                                      \
+		s8 ^= ROL32(s8, 18) ^ ROL32(s8, 22) ^ gpu_injection_constants[(j ^ 8)];                  \
+		s9 ^= ROL32(s9, 12) ^ ROL32(s9, 18) ^ gpu_injection_constants[(j ^ 9)];                  \
+		ROL_ADD(s8, s9);                                                                      \
+		s10 ^= ROL32(s10, 4) ^ ROL32(s10, 7) ^ gpu_injection_constants[(j ^ 10)];                 \
+		s11 ^= ROL32(s11, 4) ^ ROL32(s11, 31) ^ gpu_injection_constants[(j ^ 11)];                \
+		ROL_ADD(s10, s11);                                                                    \
+		s12 ^= ROL32(s12, 12) ^ ROL32(s12, 27) ^ gpu_injection_constants[(j ^ 12)];               \
+		s13 ^= ROL32(s13, 7) ^ ROL32(s13, 17) ^ gpu_injection_constants[(j ^ 13)];                \
+		ROL_ADD(s12, s13);                                                                    \
+		s14 ^= ROL32(s14, 7) ^ ROL32(s14, 8) ^ gpu_injection_constants[(j ^ 14)];                 \
+		s15 ^= ROL32(s15, 1) ^ ROL32(s15, 13) ^ gpu_injection_constants[(j ^ 15)];                \
+		ROL_ADD(s14, s15); \
+	} \
+}
+
+__constant__ uint32_t gpu_injection_constants[688] = INJECT_MAT;
+
+#define squeeze(s, k) {\
+    ((uint32_t *)output)[k] = (s); \
+}
+
+
+struct GPU_DEVICE
+{
+	uint32_t   state[N];
+	uint32_t   nonce_id;
+	uint8_t    *target;
+	uint32_t    *g_state;
+	uint8_t    *g_target;
+	uint32_t   *g_nonce_id;
+};
+
+GPU_DEVICE *gpu_divices[MAX_GPU_NUM] = {NULL};
+uint32_t gpu_divices_cnt = 0;
+
+
+__global__ void g_eaglesong(uint32_t *state, uint8_t* target, uint32_t *nonce_id)
+{
+	uint32_t global_id = blockDim.x * blockIdx.x + threadIdx.x;
+
+	uint32_t id = global_id % THREADS_PER_BLOCK;
+	uint32_t tmp;
+	uint32_t s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15;
+	uint8_t output[OUTPUT_LEN];
+
+	__shared__ uint32_t shared_state[N];
+	__shared__ uint8_t shared_target[OUTPUT_LEN];
+
+	if (id < N) shared_state[id] = state[id]; 
+	if (id < OUTPUT_LEN) shared_target[id] = target[id];
+	__syncthreads();
+
+	s0 = shared_state[0] ^ (global_id+1);
+	s1 = shared_state[1]; s2 = shared_state[2]; s3 = shared_state[3];
+	s4 = shared_state[4]; s5 = shared_state[5]; s6 = shared_state[6]; s7 = shared_state[7];
+	s8 = s9 = s10 = s11 = s12 = s13 = s14 = s15 = 0;
+	
+	EaglesongPermutation();
+	
+	s0 ^= shared_state[8]; s1 ^= shared_state[9]; s2 ^= shared_state[10];
+	
+	EaglesongPermutation();
+
+	squeeze(s0, 0); squeeze(s1, 1); squeeze(s2, 2); squeeze(s3, 3);
+	squeeze(s4, 4); squeeze(s5, 5); squeeze(s6, 6); squeeze(s7, 7);
+
+	for(int k=0; k<32; ++k) {
+		if(output[k] < shared_target[k]) {
+			atomicExch(nonce_id, global_id+1);
+		} else if(output[k] > shared_target[k]) {
+			break;
+		}
+	}
+}
+
+int gpu_hash(uint32_t gpuid)
+{
+	if (HASH_NUM > MAX_HASH_NUM) {
+		printf("HASH_NUM out of bound!!!\n");
+		return 0;
+	}
+
+	if (gpu_divices[gpuid]->g_state == NULL)
+	{
+		if (cudaMalloc((void **)&gpu_divices[gpuid]->g_state, sizeof(gpu_divices[gpuid]->state)) != cudaSuccess) {
+			printf("E01: cuda alloc memory error for state\n");
+			return 0;
+		}
+	}
+
+	if (gpu_divices[gpuid]->g_nonce_id == NULL) 
+	{
+		if (cudaMalloc((void **)&gpu_divices[gpuid]->g_nonce_id, sizeof(gpu_divices[gpuid]->nonce_id)) != cudaSuccess) {
+			printf("E02: cuda alloc memory error for nonce\n");
+			return 0;
+		}
+	}
+
+	if (gpu_divices[gpuid]->g_target == NULL)
+	{
+		if (cudaMalloc((void **)&gpu_divices[gpuid]->g_target, OUTPUT_LEN) != cudaSuccess) {
+			printf("E03: cuda alloc memory error for target\n");
+			return 0;
+		}
+	}
+
+	if (cudaMemcpy(gpu_divices[gpuid]->g_state, gpu_divices[gpuid]->state, sizeof(gpu_divices[gpuid]->state), cudaMemcpyHostToDevice) != cudaSuccess)
+	{
+		printf("E04: copy memory error for state\n");
+		return 0;
+	}
+
+	if (cudaMemcpy(gpu_divices[gpuid]->g_target, gpu_divices[gpuid]->target, OUTPUT_LEN, cudaMemcpyHostToDevice) != cudaSuccess)
+	{
+		printf("E05: copy memory error for target\n");
+		return 0;
+	}
+
+	if (cudaMemcpy(gpu_divices[gpuid]->g_nonce_id, &(gpu_divices[gpuid]->nonce_id), sizeof(gpu_divices[gpuid]->nonce_id), cudaMemcpyHostToDevice) != cudaSuccess)
+	{
+		printf("E06: copy memory error for nonce\n");
+		return 0;
+	}
+
+	g_eaglesong << <HASH_NUM / THREADS_PER_BLOCK, THREADS_PER_BLOCK >> >(gpu_divices[gpuid]->g_state, gpu_divices[gpuid]->g_target, gpu_divices[gpuid]->g_nonce_id);
+	cudaDeviceSynchronize();
+
+	if (cudaMemcpy(&(gpu_divices[gpuid]->nonce_id), gpu_divices[gpuid]->g_nonce_id, sizeof(gpu_divices[gpuid]->nonce_id), cudaMemcpyDeviceToHost) != cudaSuccess)
+	{
+		printf("E07: copy memory error for g_nonce_id\n");
+		return 0;
+	}
+
+	return HASH_NUM;
+}
+
+GPU_DEVICE* New_GPU_DEVICE()
+{
+	GPU_DEVICE* p = NULL;
+	p = (GPU_DEVICE*)malloc(sizeof(GPU_DEVICE));
+	if (p != NULL)
+	{
+		p->g_target = NULL;
+		p->g_nonce_id = NULL;
+		p->g_state = NULL;
+		p->g_target = NULL;
+	} else {
+		printf("E08: alloc memory error!\n");
+	}
+	return p;
+}
+
+void RESET_GPU_DEVICE(uint32_t gpuid)
+{
+	
+	memset(gpu_divices[gpuid]->state, 0, sizeof(gpu_divices[gpuid]->state));
+	gpu_divices[gpuid]->nonce_id = 0;
+
+	cudaFree(gpu_divices[gpuid]->g_nonce_id);
+	cudaFree(gpu_divices[gpuid]->g_state);
+	cudaFree(gpu_divices[gpuid]->g_target);
+	
+	gpu_divices[gpuid]->target = NULL;
+	gpu_divices[gpuid]->g_nonce_id = NULL;
+	gpu_divices[gpuid]->g_state = NULL;
+	gpu_divices[gpuid]->g_target = NULL;
+}
+
+void GPU_Count()
+{
+	int num;
+	cudaDeviceProp prop;
+	cudaGetDeviceCount(&num);
+	printf("deviceCount := %d\n", num);
+	gpu_divices_cnt = 0;
+	for (int i = 0; i<num; i++)
+	{
+
+		cudaGetDeviceProperties(&prop, i);
+		printf("name:%s\n", prop.name);
+		printf("totalGlobalMem:%lu GB\n", prop.totalGlobalMem / 1024 / 1024 / 1024);
+		printf("multiProcessorCount:%d\n", prop.multiProcessorCount);
+		printf("maxThreadsPerBlock:%d\n", prop.maxThreadsPerBlock);
+		printf("sharedMemPerBlock:%lu KB\n", prop.sharedMemPerBlock/1024);
+		printf("major:%d,minor:%d\n", prop.major, prop.minor);
+		gpu_divices_cnt++;
+	}
+	if (gpu_divices_cnt > MAX_GPU_NUM)gpu_divices_cnt = MAX_GPU_NUM;
+}
+
+extern "C" {
+	uint32_t c_solve_gpu(uint8_t *input, uint8_t *target, uint64_t *nonce, uint32_t gpuid) {
+		while(!gpu_divices[gpuid]) {
+			gpu_divices[gpuid] = New_GPU_DEVICE();
+		}
+
+		uint32_t ret;
+		gpu_divices[gpuid]->state[0]=(（*nonce）>>32)&(0x00000000ffffffff)
+		gpu_divices[gpuid]->state[1]=（*nonce）&(0x00000000ffffffff);
+		
+		// absorbing
+		for(int j = 0, k=0; j <= M; ++j) {
+			uint32_t sum = 0;
+			for(int v=0; v < 4; ++v) {
+				if(k < INPUT_LEN) {
+					sum = (sum << 8) ^ input[k];
+				} else if(k == INPUT_LEN) {
+					sum = (sum << 8) ^ DELIMITER;
+				}
+				++k;
+			}
+			gpu_divices[gpuid]->state[j+2] = sum;
+		}
+		gpu_divices[gpuid]->target = target;
+		gpu_divices[gpuid]->nonce_id = 0;
+
+		ret = gpu_hash(gpuid);
+
+		if(gpu_divices[gpuid]->nonce_id) {
+			*nonce = le32toh(htobe32(gpu_divices[gpuid]->state[1]));
+			*nonce = (*nonce << 32) ^ le32toh(htobe32(((gpu_divices[gpuid]->state[0])^(gpu_divices[gpuid]->nonce_id))));
+		}
+
+		return ret;
+	}
+}
---
+++ equi/eaglesong_avx2.c
@@ -0,0 +1,118 @@
+#include <immintrin.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include "portable_endian.h"
+#include "eaglesong.h"
+
+#define INPUT_LEN (32)
+#define ROUND (43)
+#define RATE (256)
+#define M (INPUT_LEN >> 2)
+#define LEN (RATE >> 3)
+#define DELIMITER (0x06)
+#define OUTPUT_LENGTH (256 >> 3)
+#define N 1600000
+
+
+#define SL(x, b)     _mm256_slli_epi32 ((x), (b))
+#define SR(x, b)     _mm256_srli_epi32 ((x), (b))
+#define XOR(a, b)    _mm256_xor_si256 ((a), (b))
+#define OR(a, b)     _mm256_or_si256 ((a), (b))
+#define ADD(a, b)    _mm256_add_epi32 ((a), (b))
+#define SET1(a)      _mm256_set1_epi32 ((a))
+#define SET          _mm256_set_epi32
+#define u256         __m256i
+#define ZERO         _mm256_setzero_si256()
+
+#define ROL32(x, b)  OR(SL((x),(b)), SR((x),(32-(b))))
+#define ROL_ADD(a,b) a = ADD(a, b); a = ROL32(a, 8); b = ADD(ROL32(b, 24) ,a);
+#define ROL_XOR(t, a, b, k) XOR(XOR(XOR(t, ROL32(t, a)), ROL32(t, b)), SET1(injection_constants_1[k]))
+
+uint32_t injection_constants_1[] = INJECT_MAT;
+
+#define EaglesongPermutation() { \
+    for(int i = 0, k=0; i < ROUND ; ++i ) { \
+        tmp = XOR(XOR(XOR(s0,s4),s12),s15); s0 = XOR(XOR(XOR(tmp,s5),s6),s7); s1 = XOR(XOR(XOR(tmp,s1),s8),s13); \
+        tmp = XOR(XOR(XOR(s1,s2),s6),s14); s2 = XOR(XOR(XOR(tmp,s7),s8),s9); s3 = XOR(XOR(XOR(tmp,s3),s10),s15); \
+        tmp = XOR(XOR(XOR(s0,s3),s4),s8); s4 = XOR(XOR(XOR(tmp,s9),s10),s11); s5 = XOR(XOR(XOR(tmp,s1),s5),s12); \
+        tmp = XOR(XOR(XOR(s2,s5),s6),s10); s6 = XOR(XOR(XOR(tmp,s11),s12),s13); s7 = XOR(XOR(XOR(tmp,s3),s7),s14); \
+        tmp = XOR(XOR(XOR(s4,s7),s8),s12); s8 = XOR(XOR(XOR(tmp,s13),s14),s15); s9 = XOR(XOR(XOR(tmp,s0),s5),s9); \
+        tmp = XOR(XOR(XOR(s6,s9),s10),s14); s10 = XOR(XOR(XOR(tmp,s0),s1),s15); s11 = XOR(XOR(XOR(tmp,s2),s7),s11); \
+        tmp = XOR(XOR(XOR(s0,s8),s11),s12); s12 = XOR(XOR(XOR(tmp,s1),s2),s3); s13 = XOR(XOR(XOR(tmp,s4),s9),s13); \
+        tmp = XOR(XOR(XOR(s3,s5),s13),s14); s14 = XOR(XOR(XOR(tmp,s2),s4),s10); s15 = XOR(XOR(XOR(XOR(XOR(XOR(XOR(tmp,s0),s1),s6),s7),s8),s9),s15); \
+        s0 = ROL_XOR(s0, 2, 4, k); ++k; s1 = ROL_XOR(s1, 13, 22, k); ++k; ROL_ADD(s0, s1); \
+        s2 = ROL_XOR(s2, 4, 19, k); ++k; s3 = ROL_XOR(s3, 3, 14, k); ++k; ROL_ADD(s2, s3); \
+        s4 = ROL_XOR(s4, 27, 31, k); ++k; s5 = ROL_XOR(s5, 3, 8, k); ++k; ROL_ADD(s4, s5); \
+        s6 = ROL_XOR(s6, 17, 26, k); ++k; s7 = ROL_XOR(s7, 3, 12, k); ++k; ROL_ADD(s6, s7); \
+        s8 = ROL_XOR(s8, 18, 22, k); ++k; s9 = ROL_XOR(s9, 12, 18, k); ++k; ROL_ADD(s8, s9); \
+        s10 = ROL_XOR(s10, 4, 7, k); ++k; s11 = ROL_XOR(s11, 4, 31, k); ++k; ROL_ADD(s10, s11); \
+        s12 = ROL_XOR(s12, 12, 27, k); ++k; s13 = ROL_XOR(s13, 7, 17, k); ++k; ROL_ADD(s12, s13); \
+        s14 = ROL_XOR(s14, 7, 8, k); ++k; s15 = ROL_XOR(s15, 1, 13, k); ++k; ROL_ADD(s14, s15); \
+    } \
+}
+
+#define squeeze(s, k) {\
+    ans = (uint32_t *)&s; \
+    for(int i=0; i < 8; ++i) { \
+        ((uint32_t *)output[i])[k] = htole32(ans[i]); \
+    } \
+}
+
+uint32_t c_solve_avx2(uint8_t *input, uint8_t *target, uint64_t *nonce) {
+    u256 s0,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15;
+    u256 state[11];
+    uint32_t rand1, rand2;
+    u256 flag, tmp;
+    uint8_t output[8][32];
+    uint32_t *ans;
+    RAND_bytes((uint8_t*) &rand1, 4);
+    RAND_bytes((uint8_t*) &rand2, 4);
+    flag = SET(7,6,5,4,3,2,1,0);
+    state[0] = SET1(rand1);
+    state[1] = SET1(rand2);
+    
+    // absorbing
+    for(int j = 0, k=0; j <= M; ++j) {
+        uint32_t sum = 0;
+        for(int v=0; v < 4; ++v) {
+            if(k < INPUT_LEN) {
+                sum = (sum << 8) ^ input[k];
+            } else if(k == INPUT_LEN) {
+                sum = (sum << 8) ^ DELIMITER;
+            }
+            ++k;
+        }
+        state[j+2] = SET1(sum);
+    }
+
+    for(uint32_t i=0; i<N; i+=8) {
+        s0 = XOR(state[0], ADD(flag, SET1(i)));
+        s1 = state[1]; s2 = state[2]; s3 = state[3];
+        s4 = state[4]; s5 = state[5]; s6 = state[6]; s7 = state[7];
+        s8 = s9 = s10 = s11 = s12 = s13 = s14 = s15 = ZERO;
+        
+        EaglesongPermutation();
+        
+        s0 = XOR(s0, state[8]); s1 = XOR(s1, state[9]); s2 = XOR(s2, state[10]);
+        
+        EaglesongPermutation();
+
+        squeeze(s0, 0); squeeze(s1, 1); squeeze(s2, 2); squeeze(s3, 3);
+        squeeze(s4, 4); squeeze(s5, 5); squeeze(s6, 6); squeeze(s7, 7);
+
+        for(int j=0; j<8; ++j) {
+            for(int k=0; k<32; ++k) {
+                if(output[j][k] < target[k]) {
+                    *nonce = le32toh(htobe32(rand2));
+                    *nonce = (*nonce << 32) ^ le32toh(htobe32((rand1^(i|j))));
+                    return i+8;
+                } else if(output[j][k] > target[k]) {
+                    break;
+                }
+            }
+        }
+    }
+
+    return N;
+}
